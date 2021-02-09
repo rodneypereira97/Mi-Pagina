8ff92f119601cda7ae4fd5b95f3e10969954b5f6
Rodney <rodpereira@gmail.com>
2021-02-09 16:32:05 -0300

    modificado de nuevo ir arriba v2.0.2

--- js/main.js
+++ js/main.js
@@ -29,20 +29,3 @@ document.querySelectorAll(".hamburguer")[0].addEventListener("click", function()
     
     enlacesHeader.classList.toggle("menudos")
 })
-$(document).ready(function(){
- 
-    $('.ir-arriba').click(function(){
-        $('body, html').animate({
-            scrollTop: '0px'
-        }, 300);
-    });
-   
-    $(window).scroll(function(){
-        if( $(this).scrollTop() > 0 ){
-            $('.ir-arriba').slideDown(300);
-        } else {
-            $('.ir-arriba').slideUp(300);
-        }
-    });
-   
-   });
